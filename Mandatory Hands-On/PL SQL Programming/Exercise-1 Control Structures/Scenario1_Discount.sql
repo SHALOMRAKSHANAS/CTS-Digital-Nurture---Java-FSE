638f283b67ec2628378d9a1a9d49c775961a3963
SHALOMRAKSHANAS <tstr2133.shalomrakjshanas@gmail.com>
2026-06-22 22:56:57 +0530

    Added PL SQL Control Structures Exercise

---
+++ Mandatory Hands-On/PL SQL Programming/Exercise-1 Control Structures/Scenario1_Discount.sql
@@ -0,0 +1,20 @@
+DECLARE
+CURSOR cust_cursor IS
+SELECT CustomerID, Age
+FROM Customers;
+
+BEGIN
+FOR cust_rec IN cust_cursor LOOP
+IF cust_rec.Age > 60 THEN
+UPDATE Loans
+SET InterestRate = InterestRate - 1
+WHERE CustomerID = cust_rec.CustomerID;
+END IF;
+END LOOP;
+
+```
+COMMIT;
+```
+
+END;
+/
